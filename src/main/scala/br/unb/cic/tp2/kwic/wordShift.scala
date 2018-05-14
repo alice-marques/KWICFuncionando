d63650a00abf3fe8c2d05a1ab95bcc373f86d9f6
Andre Marques <andre.acm97@outlook.com>
2018-05-14 20:25:26 -0300

    Versão com opção de escolher o nome do arquivo

--- src/main/scala/br/unb/cic/tp2/kwic/wordShift.scala
+++ src/main/scala/br/unb/cic/tp2/kwic/wordShift.scala
@@ -2,7 +2,7 @@ package br.unb.cic.tp2.kwic
 import java.io.PrintWriter
 object wordShift {
   val target = 1
-  val normalize = 15
+  val normalize = 20
   def printTitles(): Unit ={
     var aux:String = ""
     for(title <- titlesManager.cleanedTitles){
@@ -35,8 +35,15 @@ object wordShift {
         }
       }
     }
-  def printTitlesNormalizedToFile()={
-    val writer = new PrintWriter("Saida.txt")
+  def printTitlesNormalizedToFile():Unit={
+    println("Please select 1 to write to the default file or 2 to write to a file of your choice:")
+    var writer = new PrintWriter("Saida.txt")
+    scala.io.StdIn.readInt() match{
+      case 1 =>
+      case 2 => println("Please write the name of the file:");writer.close(); writer = new PrintWriter(scala.io.StdIn.readLine())
+      case _ => println("Please choose a valid option."); printTitlesNormalizedToFile()
+    }
+
     var aux:String = ""
     for(title <- titlesManager.cleanedTitles){
       for(word <- title){
